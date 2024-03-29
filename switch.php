0c0709bdc4945ff7c40b9716f25e50cc8b5104c0
Barnav Pran Kalita <barnavpran12345@gmail.com>
2024-03-29 23:46:03 +0530

    add files via upload

--- switch.php
+++ switch.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<head>
+    <title>SWITCH_CASE</title>
 </head>
 <body>
     <form method="POST" action="switch.php">
